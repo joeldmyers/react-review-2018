a3b47c6e9cfd0e49d0a1cea80a456ba967b824d0
Joel Myers <joeldmyers@gmail.com>
2018-08-11 17:28:42 -0400

    adding search bar component, getting it into main app

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// my components
+import SearchBar from './components/search_bar';
+
 const YOUTUBE_API_KEY = 'AIzaSyB1ACzPBlhVNVlHxFZgPiJHo4Q9riuV9PI';
 // Make first component
 const App = () => {
-  return <div>hello</div>;
+  return <div>
+    <SearchBar />
+      hello
+    </div>;
 }
 
 // render it
