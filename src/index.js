3a7bb748f6a73c5bc7e4b048c429e72ec3321fda
Joel Myers <joeldmyers@gmail.com>
2018-08-11 17:19:40 -0400

    installing youtube search and adding API key

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const YOUTUBE_API_KEY = 'AIzaSyB1ACzPBlhVNVlHxFZgPiJHo4Q9riuV9PI';
 // Make first component
 const App = () => {
   return <div>hello</div>;
