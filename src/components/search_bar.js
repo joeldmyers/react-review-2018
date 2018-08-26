247e2671e287feb5b4dc0a265c2270644a2262d0
Joel Myers <joeldmyers@gmail.com>
2018-08-26 12:34:53 -0400

    initializing state for search term

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,6 +2,14 @@ import React, {Component} from 'react';
 
 class SearchBar extends Component {
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      term: ''
+    };
+  }
+
   onInputChange(e) {
     console.log(e);
   }
